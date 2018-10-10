a9f7ba709c374f1f407c66f1862baedf7bcd28d3
Firework98 <to.sengpiel@gmail.com>
2018-10-10 20:45:58 +0200

    Edited Vehicle

--- src/de/trafficsim/logic/vehicles/Vehicle.java
+++ src/de/trafficsim/logic/vehicles/Vehicle.java
@@ -1,10 +1,19 @@
 package de.trafficsim.logic.vehicles;
 
 import de.trafficsim.logic.streets.tracks.Track;
+import de.trafficsim.util.geometry.Position;
 
 import java.util.List;
 
 public class Vehicle {
 
+    protected double velocity;
     protected List<Track> path;
+
+    public Position getPosition(){
+        return new Position(0,0);
+    }
+    public Position getDirection(){
+        return new Position(1,0);
+    }
 }
